291acc76e8068a37cadbd886636c7f4d23fcdfde
Dario Cangialosi @Arkenidar <dario.cangialosi@gmail.com>
2026-02-18 12:30:43 +0100

    refactor: Dynamically load code-decorations.js and code-decorations.css for improved modularity

--- web/show-source.js
+++ web/show-source.js
@@ -14,9 +14,6 @@ style=" overflow-y: scroll; border:1px solid black; padding: 1em; "
 spellcheck="false" translate="no">
 </pre>
 
-<link rel="stylesheet" href="/web/code-decorations.css">
-<script src="/web/code-decorations.js"></script>
-
 </div>`
 
     script.insertAdjacentHTML("afterend", html)
@@ -27,12 +24,21 @@ spellcheck="false" translate="no">
         then(response_text => {
             text_viewer.textContent = response_text
 
+            console.info(script.src)
+            const base = new URL(script.src)
+
+            const url_js = new URL("code-decorations.js", base)
+            console.info(url_js.href)
+
+            const url_css = new URL("code-decorations.css", base)
+            console.info(url_css.href)
+
             // code_decorations()
 
             // inject code-decorations.js code here
             // to color the source code in the text_viewer element
             var scriptElement = document.createElement("script")
-            scriptElement.src = "/web/code-decorations.js"
+            scriptElement.src = url_js.href // "/web/code-decorations.js"
             text_viewer.appendChild(scriptElement)
 
             // execute the code-decorations.js code to color the source code in the text_viewer element
@@ -40,6 +46,12 @@ spellcheck="false" translate="no">
             scriptElement.onload = function () {
                 code_decorations()
             }
+
+            // also inject the code-decorations.css file to style the colored source code
+            var linkElement = document.createElement("link")
+            linkElement.rel = "stylesheet"
+            linkElement.href = url_css.href // "/web/code-decorations.css"
+            document.head.appendChild(linkElement)
 
         })
 
